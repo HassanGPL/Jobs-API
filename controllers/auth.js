45fd544ce2397090b7a9737de606429ca2afe8b3
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-05 17:59:31 +0300

    Add Register Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
 const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
 
 
-exports.register = (req, res, next) => {
-    res.json(req.body);
+exports.register = async (req, res, next) => {
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED)
+        .json({ user: { name: user.name }, token });
 }
 
 exports.login = (req, res, next) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new Schema({
     name: {
@@ -24,5 +26,17 @@ const userSchema = new Schema({
         minlength: 8
     }
 });
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign(
+        { userId: this._id, name: this.name },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_LIFETIME });
+}
 
 module.exports = mongoose.model('User', userSchema);

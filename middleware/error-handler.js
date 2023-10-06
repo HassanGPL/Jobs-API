e416cfa105e2abcafb8cc9d531200c61fc03ab99
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-07 00:33:12 +0300

    Handle Validation Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,22 +2,32 @@ const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  let customError = {
-    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.msg || 'Something went wrong, try again later'
-  }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong, try again later'
+    }
 
-  if (err.code && err.code == 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please try another value`;
-    customError.statusCode = StatusCodes.BAD_REQUEST;
-  }
+    // if (err instanceof CustomAPIError) {
+    //   return res.status(err.statusCode).json({ msg: err.message })
+    // }
 
-  return res.status(customError.statusCode).json({ msg: customError.msg })
+    if (err.name === "ValidationError") {
+        customError.msg = Object
+            .values(err.errors)
+            .map(item => item.message)
+            .join(', ');
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
 
+
+
+    if (err.code && err.code == 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please try another value`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware

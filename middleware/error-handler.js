d1c88a98994eec02876f6a07c1a44c8abab6fbfa
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-07 00:45:28 +0300

    Handle Cast Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
 
     let customError = {
+        // Set Defaults
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || 'Something went wrong, try again later'
     }
@@ -12,6 +13,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     //   return res.status(err.statusCode).json({ msg: err.message })
     // }
 
+    // Handle Validation Errors
     if (err.name === "ValidationError") {
         customError.msg = Object
             .values(err.errors)
@@ -20,12 +22,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
+    // Handle Cast Errors
+    if (err.name === "CastError") {
+        customError.msg = `No Item Found with ID : ${err.value}`;
+        customError.statusCode = StatusCodes.NOT_FOUND;
+    }
 
-
+    // Handle Duplicate (Email) Errors
     if (err.code && err.code == 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please try another value`;
         customError.statusCode = StatusCodes.BAD_REQUEST;
     }
+
     // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
     return res.status(customError.statusCode).json({ msg: customError.msg })
 }

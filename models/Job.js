e3bf95f3c711e703826051f844f1ac69280792a2
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-06 19:47:09 +0300

    Create Jobs Model

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const jobSchema = new Schema({
+    company: {
+        type: String,
+        required: [true, "Please Provide Company..."],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: [true, "Please Provide Position..."],
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, "Please Provide User..."]
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Job', jobSchema);

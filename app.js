84c135fce55d1178c83c1bd1a9285bf3588b11a9
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-07 01:16:06 +0300

    Add Security Packages

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 const authUser = require('./middleware/authentication');
 
+// Extra Security Packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
+
+
 // DBConnection
 const connectDB = require('./db/connect');
 
@@ -15,8 +24,16 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // routes
 app.use('/api/v1/auth', authRouter);
